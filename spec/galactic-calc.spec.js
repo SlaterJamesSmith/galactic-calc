c12f3ab1d95e84f37b1034ed25ab86f9700f9f13
Slater Smith <smithslater@gmail.com>
2019-03-15 13:39:03 -0700

    build successful test for birthday in spec.js file

--- spec/galactic-calc.spec.js
+++ spec/galactic-calc.spec.js
@@ -1,4 +1,4 @@
-import { Birthday } from "../src/galactic-calc.js";
+// import { calc } from "../src/galactic-calc.js";
 
 describe ("Birthday", function() {
   // it('should be a "Wed Mar 13 2019"', function(){
@@ -7,50 +7,70 @@ describe ("Birthday", function() {
   //   expect(date.toString()).toEqual("Sat Oct 12 2019 00:00:00 GMT-0700 (Pacific Daylight Time)");
   // });
 
-  it('Application returns user’s current age', function(){
-    const birthday = new Birthday;
-    const input = 29;
-    expect(input).toEqual("29");
+  it('should return user’s current age', function(){
+    class Birthday {
+      constructor (year, month, day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+      }
+
+      earthAge() {
+        let date = new Date;
+        return date.getFullYear() - 1990;
+      }
+
+    }
+
+    let earth = new Birthday(1989, 10, 22);
+
+    expect(earth.earthAge()).toEqual(29);
   });
 
-  it('should return current age divided by .24', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(29 / .24);
-  });
-
-  it('should return current age divided by .62', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(29 / .62);
-  });
-
-  it('should return current age divided by 1.88', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(29 / 1.88);
-  });
-
-  it('should return current age divided by 11.86', function(){
-    // const input = (parseInt("asfaf"));
-    // expect(input).toEqual(NaN);
-  });
-
-  it('should return user’s life expectancy divided by .24', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(75 / .24);
-  });
-
-  it('should return user’s life expectancy divided by .62', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(75 / .62);
-  });
-
-  it('should return user’s life expectancy divided by 1.88', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(75 / 1.88);
-  });
-
-  it('should return user’s life expectancy divided by 11.86', function(){
-    // const input = (parseInt("asfaf"));
-    expect(input).toEqual(75 / 11.86);
-  });
-
+//   it('Application returns user’s current age', function(){
+//     const birthday = new Birthday;
+//     const input = 29;
+//     expect(input).toEqual(29);
+//   });
+//
+//   it('should return current age divided by .24', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(29 / .24);
+//   });
+//
+//   it('should return current age divided by .62', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(29 / .62);
+//   });
+//
+//   it('should return current age divided by 1.88', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(29 / 1.88);
+//   });
+//
+//   it('should return current age divided by 11.86', function(){
+//     // const input = (parseInt("asfaf"));
+//     // expect(input).toEqual(NaN);
+//   });
+//
+//   it('should return user’s life expectancy divided by .24', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(75 / .24);
+//   });
+//
+//   it('should return user’s life expectancy divided by .62', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(75 / .62);
+//   });
+//
+//   it('should return user’s life expectancy divided by 1.88', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(75 / 1.88);
+//   });
+//
+//   it('should return user’s life expectancy divided by 11.86', function(){
+//     // const input = (parseInt("asfaf"));
+//     expect(input).toEqual(75 / 11.86);
+//   });
+//
 });
